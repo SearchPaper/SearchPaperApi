8f47b19945acc1a61b5c3664d23565e5db6501d8
Lucas Pereira dos Santos <lucasdev2001@gmail.com>
2025-03-27 07:05:54 -0300

    Add: S3 Storage

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Amazon.S3;
+
 namespace SearchPaperApi;
 
 public class Program
@@ -6,11 +8,30 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        // var allowedHosts = builder.Configuration.GetValue<string>("PizzaFlavor");
+
+
         // Add services to the container.
 
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+
+        builder.Services.AddSingleton(sp =>
+        {
+            var serviceURL = builder.Configuration.GetValue<string>("AWS:ServiceURL");
+            var accessKey = builder.Configuration.GetValue<string>("AWS:AccessKey");
+            var secretKey = builder.Configuration.GetValue<string>("AWS:SecretKey");
+
+            var config = new AmazonS3Config
+            {
+                ServiceURL = serviceURL,
+                RegionEndpoint = Amazon.RegionEndpoint.USEast1,
+            };
+            var s3Client = new AmazonS3Client(accessKey, secretKey, config);
+
+            return s3Client;
+        });
 
         var app = builder.Build();
 
